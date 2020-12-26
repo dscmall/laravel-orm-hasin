f62cd3016bfd6cc7eac1af1e9fda670d16783ac3
zhuofuxi <zhuofuxi@ecmoban.com>
2020-12-26 11:50:39 +0800

    优化代码

--- src/Database/Eloquent/RelationMixin.php
+++ src/Database/Eloquent/RelationMixin.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
 use Illuminate\Database\Eloquent\Relations\MorphOneOrMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class RelationMixin
@@ -18,22 +19,22 @@ class RelationMixin
     public function getRelationInQuery(): Closure
     {
         /**
-         * @param  \Illuminate\Database\Eloquent\Builder  $query
-         * @param  \Illuminate\Database\Eloquent\Builder  $parentQuery
-         * @param  array|mixed  $columns
+         * @param  \Illuminate\Database\Eloquent\Builder $query
+         * @param  \Illuminate\Database\Eloquent\Builder $parentQuery
+         * @param  array|mixed $columns
          * @return \Illuminate\Database\Eloquent\Builder
          */
-        return function (Builder $query, Builder $parentQuery, $columns = ['*']): Builder{
+        return function (Builder $query, Builder $parentQuery, $columns = ['*']): Builder {
             // Abstract Relation
-            $relation = function (Builder $query, Builder $parentQuery, $columns = ['*']): Builder{
+            $relation = function (Builder $query, Builder $parentQuery, $columns = ['*']): Builder {
                 /** @var Relation $this */
                 $columns = $columns == ['*'] ? $this->getExistenceCompareKey() : $columns;
                 return $query->select($columns);
             };
-            $hasOneOrMany = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($relation): Builder{
+            $hasOneOrMany = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($relation): Builder {
                 return $relation($query, $parentQuery, $columns);
             };
-            $morphOneOrMany = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($hasOneOrMany): Builder{
+            $morphOneOrMany = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($hasOneOrMany): Builder {
                 /** @var MorphOneOrMany $this */
                 return $hasOneOrMany($query, $parentQuery, $columns)->where(
                     $query->qualifyColumn($this->getMorphType()), $this->morphClass
@@ -41,34 +42,34 @@ class RelationMixin
             };
             // Entity Relation
             // BelongsTo (extend Relation, overload)
-            $belongsTo = function (Builder $query, Builder $parentQuery, $columns = ['*']): Builder{
+            $belongsTo = function (Builder $query, Builder $parentQuery, $columns = ['*']): Builder {
                 /** @var BelongsTo $this */
                 $columns = $columns == ['*'] ? $query->qualifyColumn($this->ownerKey) : $columns;
 
                 return $query->select($columns);
             };
             // BelongsToMany (extend Relation, iteration)
-            $belongsToMany = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($relation): Builder{
+            $belongsToMany = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($relation): Builder {
                 /** @var BelongsToMany $this */
                 $this->performJoin($query);
 
                 return $relation($query, $parentQuery, $columns);
             };
             // HasMany (extend HasOneOrMany, inherit)
-            $hasMany = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($hasOneOrMany): Builder{
+            $hasMany = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($hasOneOrMany): Builder {
                 return $hasOneOrMany($query, $parentQuery, $columns);
             };
             // HasManyThrough (extend Relation, overload)
-            $hasManyThrough = function (Builder $query, Builder $parentQuery, $columns = ['*']): Builder{
+            $hasManyThrough = function (Builder $query, Builder $parentQuery, $columns = ['*']): Builder {
                 /** @var HasManyThrough $this */
                 $columns = $columns == ['*'] ? $this->getQualifiedFirstKeyName() : $columns;
 //                 if ($parentQuery->getQuery()->from === $query->getQuery()->from) {
-                       // TODO
+                // TODO
 //                     return $this->getRelationExistenceQueryForSelfRelation($query, $parentQuery, $columns);
 //                 }
 
 //                 if ($parentQuery->getQuery()->from === $this->throughParent->getTable()) {
-                       // TODO
+                // TODO
 //                     return $this->getRelationExistenceQueryForThroughSelfRelation($query, $parentQuery, $columns);
 //                 }
 
@@ -77,30 +78,30 @@ class RelationMixin
                 return $query->select($columns);
             };
             // HasOne (extend HasOneOrMany, inherit)
-            $hasOne = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($hasOneOrMany): Builder{
+            $hasOne = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($hasOneOrMany): Builder {
                 return $hasOneOrMany($query, $parentQuery, $columns);
             };
             // HasOneThrough (extend HasManyThrough, inherit)
-            $hasOneThrough = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($hasManyThrough): Builder{
+            $hasOneThrough = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($hasManyThrough): Builder {
                 return $hasManyThrough($query, $parentQuery, $columns);
             };
             // MorphMany (extend MorphOneOrMany, inherit)
-            $morphMany = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($morphOneOrMany): Builder{
+            $morphMany = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($morphOneOrMany): Builder {
                 return $morphOneOrMany($query, $parentQuery, $columns);
             };
             // MorphOne (extend MorphOneOrMany, inherit)
-            $morphOne = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($morphOneOrMany): Builder{
+            $morphOne = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($morphOneOrMany): Builder {
                 return $morphOneOrMany($query, $parentQuery, $columns);
             };
             // MorphTo (extend BelongsTo, inherit)
-            $morphTo = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($belongsTo): Builder{
+            $morphTo = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($belongsTo): Builder {
                 return $belongsTo($query, $parentQuery, $columns);
             };
             // MorphToMany (extend BelongsToMany, iteration)
-            $morphToMany = function (Builder $query, Builder $parentQuery, $columns = ['*'])use($belongsToMany): Builder{
+            $morphToMany = function (Builder $query, Builder $parentQuery, $columns = ['*']) use ($belongsToMany): Builder {
                 /** @var MorphToMany $this */
                 return $belongsToMany($query, $parentQuery, $columns)->where(
-                    $this->table.'.'.$this->morphType, $this->morphClass
+                    $this->table . '.' . $this->morphType, $this->morphClass
                 );
             };
             /* After all the above code is abbreviated, you can clearly see the inheritance relationship, which perfectly fits the inheritance structure of the original framework */
@@ -117,59 +118,64 @@ class RelationMixin
              * 于是乎就定义好了方法之后，根据当前类名来进行动态函数调用
              * 这一套实现我愿称之为最强！！！
              */
-            $relationName = (string)Str::of(get_class($this))->afterLast('\\')->camel();
-            return ${$relationName}($query,$parentQuery,$columns);
+            $relationName = explode('\\', get_class($this));
+            $relationName = Arr::last($relationName);
+            $relationName = (string)Str::camel($relationName);
+            return ${$relationName}($query, $parentQuery, $columns);
         };
     }
 
     public function getRelationWhereInKey(): Closure
     {
-        return function (): string{
-            $relation = function (): string{
+        return function (): string {
+            $relation = function (): string {
                 /** @var Relation $this */
                 return $this->getQualifiedParentKeyName();
             };
-            $hasOneOrMany = function ()use($relation): string{
+            $hasOneOrMany = function () use ($relation): string {
                 return $relation();
             };
-            $morphOneOrMany = function ()use($hasOneOrMany): string{
+            $morphOneOrMany = function () use ($hasOneOrMany): string {
                 return $hasOneOrMany();
             };
 
-            $belongsTo = function (): string{
+            $belongsTo = function (): string {
                 /** @var BelongsTo $this */
                 return $this->getQualifiedForeignKeyName();
             };
-            $belongsToMany = function ()use($relation): string{
+            $belongsToMany = function () use ($relation): string {
                 return $relation();
             };
-            $hasMany = function ()use($hasOneOrMany): string{
+            $hasMany = function () use ($hasOneOrMany): string {
                 return $hasOneOrMany();
             };
-            $hasManyThrough = function (): string{
+            $hasManyThrough = function (): string {
                 /** @var HasManyThrough $this */
                 return $this->getQualifiedLocalKeyName();
             };
-            $hasOne = function ()use($hasOneOrMany): string{
+            $hasOne = function () use ($hasOneOrMany): string {
                 return $hasOneOrMany();
             };
-            $hasOneThrough = function ()use($hasManyThrough): string{
+            $hasOneThrough = function () use ($hasManyThrough): string {
                 return $hasManyThrough();
             };
-            $morphMany = function ()use($morphOneOrMany): string{
+            $morphMany = function () use ($morphOneOrMany): string {
                 return $morphOneOrMany();
             };
-            $morphOne = function ()use($morphOneOrMany): string{
+            $morphOne = function () use ($morphOneOrMany): string {
                 return $morphOneOrMany();
             };
-            $morphTo = function ()use($belongsTo): string{
+            $morphTo = function () use ($belongsTo): string {
                 return $belongsTo();
             };
-            $morphToMany = function ()use($belongsToMany): string{
+            $morphToMany = function () use ($belongsToMany): string {
                 return $belongsToMany();
             };
 
-            $relationName = (string)Str::of(get_class($this))->afterLast('\\')->camel();
+            $relationName = explode('\\', get_class($this));
+            $relationName = Arr::last($relationName);
+            $relationName = (string)Str::camel($relationName);
+
             return ${$relationName}();
         };
     }
